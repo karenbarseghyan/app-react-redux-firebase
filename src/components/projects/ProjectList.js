e58219bb43c1cdd26dc797d704f7164d72a95b73
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-01-22 00:54:49 +0400

    Project Summary: making project list modular

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
     return (
         <div className = "project list section">
-
-            <div className = "card project-summary purple lighten-5">
-                <div className = "card-content grey-text text-darken-3">
-                    <span className = "card-title red-text"> Project Title 1 </span>
-                    <p>Posted By Karen</p>
-                    <p className="grey-text">Today, 1 a.m</p>
-                </div>
-            </div>
-
-            <div className = "card project-summary purple lighten-5">
-                <div className = "card-content grey-text text-darken-3">
-                    <span className = "card-title red-text"> Project Title 2 </span>
-                    <p>Posted By Christina</p>
-                    <p className="grey-text">Today, 2 a.m</p>
-                </div>
-            </div>
-
-            <div className = "card project-summary purple lighten-5">
-                <div className = "card-content grey-text text-darken-3">
-                    <span className = "card-title red-text"> Project Title 3 </span>
-                    <p>Posted By Ruben</p>
-                    <p className="grey-text">Today, 3 a.m</p>
-                </div>
-            </div>
-            
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />       
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className = "card project-summary purple lighten-5">
+            <div className = "card-content grey-text text-darken-3">
+                <span className = "card-title red-text"> Project Title 1 </span>
+                <p>Posted By Karen</p>
+                <p className="grey-text">Today, 1 a.m</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
