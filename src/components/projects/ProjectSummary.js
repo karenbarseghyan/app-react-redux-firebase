bc154838412934e897eafd00a4474f5dffd1b80f
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-01-23 00:46:51 +0400

    Adding Dummy Data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = (project) => {
     return (
         <div className = "card project-summary purple lighten-5">
             <div className = "card-content grey-text text-darken-3">
-                <span className = "card-title red-text"> Project Title 1 </span>
+                <span className = "card-title red-text"> {project.title}</span>
                 <p>Posted By Karen</p>
                 <p className="grey-text">Today, 1 a.m</p>
             </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'search me', content: 'blah blah'},
+        {id: '2', title: 'find me', content: 'blah blah'},
+        {id: '3', title: 'help me', content: 'blah blah'}
+    ]
+}
 
 const projectReducer = (state = initState,action) => {
     return state;
