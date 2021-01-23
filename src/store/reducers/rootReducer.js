4ac6e2c9bde375d4ac93fab4fa6498779eed023e
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-01-24 00:25:00 +0400

    Getting data from Firestore

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from './authReducer.js'
 import projectReducer from './projectReducer.js'
 import {combineReducers} from 'redux'
+import {firestoreReducer} from 'redux-firestore'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project : projectReducer,
+    firestore: firestoreReducer
 });
 
 export default rootReducer
